9f6e8e2df8bd8de2258fa28341a8a2c24567a956
IndoorKi8 <100283987+IndoorKi8@users.noreply.github.com>
2022-05-11 15:05:24 +0530

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from flask import abort, json, jsonify, request, Flask, make_response
+from datetime import datetime
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/info',methods = ['GET'])
+def info():
+  res = {}
+  res['msg'] = 'App works succesfully'
+  response = make_response(res)
+  return response
